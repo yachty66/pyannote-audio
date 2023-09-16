6740db2b8fe0d571b889147df7a2b85edbc3602f
Hervé BREDIN <hbredin@users.noreply.github.com>
2023-09-16 15:50:40 +0200

    fix(core): fix Model.example_output for embedding models

--- pyannote/audio/core/model.py
+++ pyannote/audio/core/model.py
@@ -198,12 +198,13 @@ class Model(pl.LightningModule):
             example_output: torch.Tensor,
             specifications: Specifications = None,
         ) -> Output:
-            _, num_frames, dimension = example_output.shape
-
             if specifications.resolution == Resolution.FRAME:
+                _, num_frames, dimension = example_output.shape
                 frame_duration = specifications.duration / num_frames
                 frames = SlidingWindow(step=frame_duration, duration=frame_duration)
             else:
+                _, dimension = example_output.shape
+                num_frames = None
                 frames = None
 
             return Output(
